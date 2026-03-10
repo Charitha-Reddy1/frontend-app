0bd544fa3be435fc3eeea70d1bec3d49d74faf2d
Charitha-Reddy1 <carg2800@gmail.com>
2026-03-10 15:03:03 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+import './Footer.css'
+
+function Footer(){
+    return <h3 className='App-Footer'>Copyright 2026.All rights reserved.</h3>
+}
+
+export default Footer
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import './Header.css'
+
+function Header(){
+    return <div class='App-Header'>
+                <h1>My React Application</h1>
+                <ol>
+                    <li>Home</li>
+                    <li>Products</li>
+                    <li>Users</li>
+                    <li>Login</li>
+                    <li>Logout</li>
+                </ol>
+            </div>
+}
+
+export default Header

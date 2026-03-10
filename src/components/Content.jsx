2840f08f5dbde9d2bffaed39bad0acd362d2b194
Charitha-Reddy1 <carg2800@gmail.com>
2026-03-10 21:36:28 +0530

    Add product fetching functionality and update state management in Content component

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,5 +1,38 @@
+import {useState,useEffect} from 'react'
+import axios from 'axios'
+
 function Content(){
-    return <h3>Products Page</h3>
+    const [count,setCount]=useState(0)
+    const [products,setProducts]=useState([])
+
+    const inc=()=>{
+        setCount(count+1);
+    }
+
+    const dec=()=>{
+        setCount(count-1);
+    }
+
+    const fetchProducts=async()=>{
+        const url='http://localhost:5000/products'
+        const res=await axios.get(url)
+        setProducts(res.data)
+    }
+
+    useEffect (fetchProducts(),[])
+
+    return (<div>
+        <h3>Products Page</h3>
+        <h1>
+        <button onClick={dec}>-</button> {count} 
+        <button onClick={inc}>+</button>
+        </h1>
+        <br/>
+        {products.map(product=>{
+            <li>product.name</li>
+        })}
+        </div>
+    );
 }
 
 export default Content

8da912f8b1900f51e6984ae34622db59086c2733
Charitha-Reddy1 <carg2800@gmail.com>
2026-03-28 12:04:20 +0530

    registerkey

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -7,21 +7,39 @@ function Register(){
     const [user,setUser]=useState({})
     const API_URL=import.meta.env.VITE_API_URL
     const Navigate=useNavigate()
-    const handleSubmit=async ()=>{
+    const handleSubmit=async (e)=>{
+        e.preventDefault()
+
+    try {
+
         const url=API_URL+"/auth/signup"
         const res= await axios.post(url,user)
+
+        if (response.data.error) {
+            alert(response.data.error)
+            return
+        }
+
         Navigate("/login")
-    };
+
+    }
+    catch (error) {
+    console.log(error)
+    alert("Registration failed")
+    }
+}
     return (
+    <form  onSubmit={handleSubmit}>
     <div>
     <h1>Registration Page</h1>
     <p><input type="text" onChange={(e)=>setUser({...user,name:e.target.value})} placeholder="Name"></input></p>
     <p><input type="text" onChange={(e)=>setUser({...user,email:e.target.value})} placeholder="Email"></input></p>
     <p><input type="password" onChange={(e)=>setUser({...user,password:e.target.value})} placeholder="Password"></input></p>
-    <p><button onClick={handleSubmit}>Submit</button></p>
+    <p><button type="submit">Submit</button></p>
     <p>Already a User?👇🏻</p>
     <p><Link to='/login'>Click to Login Here</Link></p>
     </div>
+    </form>
 )
 }
 
